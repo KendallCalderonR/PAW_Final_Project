e673d2e48ebc77a79dab1acb67a365298a2bbbd9
Kendall Calderon Rodriguez <kendall-1132@hotmail.com>
2018-12-08 18:43:15 -0600

    Logic agregar categoria y subcategoria

---
+++ PAW.FinalProject.WCF/Logic/Specification/Metodos_De_Especificacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PAW.FinalProject.WCF.Logic.Specification
+{
+    public class Metodos_De_Especificacion
+    {
+
+        public DB.Category AgregarCategoria(string Categoria)
+        {
+            var Action = new Action.Metodos_De_Accion();
+            var Result = Action.AgregarCategoria(Categoria);
+            return Result;
+        }
+
+        public DB.SubCategory AgregarSubCategoria(string SubCategoria)
+        {
+            var Action = new Action.Metodos_De_Accion();
+            var Result = Action.AgregarSubCategoria(SubCategoria);
+            return Result;
+        }
+    }
+}
