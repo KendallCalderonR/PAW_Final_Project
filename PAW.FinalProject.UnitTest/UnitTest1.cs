50ec91d64f35e0e4d4724d342f72e9e8c7b9ce5e
Ismael Gonzalez <ijgr11@gmail.com>
2018-12-15 21:44:44 -0600

    .

--- PAW.FinalProject.UnitTest/UnitTest1.cs
+++ PAW.FinalProject.UnitTest/UnitTest1.cs
@@ -6,7 +6,6 @@ namespace PAW.FinalProject.UnitTest
     [TestClass]
     public class UnitTest1
     {
-        //initiate all testing methods
         [TestMethod]
         public void TestMethod1()
         {
